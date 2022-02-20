02b9e546d0f5cf405c34a777a2e99e136239af3e
Nasir Sunny <63219166+NasirSunny50@users.noreply.github.com>
2022-02-20 23:05:03 +0600

    Modules Added

---
+++ modules/config.py
@@ -0,0 +1,19 @@
+
+#=================================\CONFIG./=====================================
+# default System camera access code = 1, if connect with any external camera put 1,2 and so on with the no of connected cameras.
+camera_no = 0
+
+
+
+People_Counter = True
+
+
+Threshold = 15
+
+
+USE_GPU = True
+
+MIN_CONF = 0.3
+
+NMS_THRESH = 0.3
+#===============================================================================
